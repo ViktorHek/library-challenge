0df7e8de6d50524132dc48f41aa3a19c16d10b4a
viktor <viktorkarlsson125@gmail.com>
2020-10-30 23:00:43 +0100

    på något sätt lyckades jag koppla data.rb till library.yaml, men av någon anledning får jag bara andra :item.

--- lib/data.rb
+++ lib/data.rb
@@ -2,9 +2,10 @@ require 'yaml'
 
 # kod jag såg på youtube
 book_list = YAML.load(File.open(File.join(File.dirname(__FILE__),'library.yaml')))
-#alternativt 
-book_list2 = YAML.load_file('library.yaml')
 
-puts book_list2
+#alternativt 
+# book_list2 = YAML.load_file('library.yaml')
+
+puts book_list
 # test för att se om book_list funkar, hittills funkar ingen  
 # error meddelande lib/library.yaml:2: syntax error, unexpected ':', expecting end-of-input :item:
