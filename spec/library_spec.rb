8b2540e51a11c4357fbe649d987799293d4cf642
viktor <viktorkarlsson125@gmail.com>
2020-10-30 15:58:56 +0100

    skapat test och implementerings fil

---
+++ spec/library_spec.rb
@@ -0,0 +1,11 @@
+
+describe library do 
+    let(:account) { instance_double('Account', pin_code: '1234', exp_date: '04/22', account_status: :active) }
+
+# create instance_double
+
+# if book is available, return true
+
+# if book is not available return false
+
+# if book false return 'return_date'
